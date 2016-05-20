f8fb84cd201652b0f515dde2b73be89e363d5b6a
SoleMina <karina.pradogutierrez@gmail.com>
2016-05-20 15:14:09 -0500

    trabcoders[

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var aleatorio =  Math.floor(Math.random()*40);
+
+$("#c-box").css("background-image", "url('./fotos/peru/"+peru[aleatorio].image+"')")
+
+$( document ).ready(function() {
+    $("#selector").change(function() {
+      var pais =$(this).val();
+      if (pais == 1) {
+        aleatorio =  Math.floor(Math.random()*33);
+        $("#c-box").css("background-image", "url('./fotos/peru/"+peru[aleatorio].image+"')")
+      } else if (pais == 3) {
+        aleatorio =  Math.floor(Math.random()*33);
+        $("#c-box").css("background-image", "url('./fotos/mexico/"+mexico[aleatorio].image+"')")
+      }
+    })
+});
